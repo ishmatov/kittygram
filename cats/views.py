84befa50fd93274f817a0372f3910a9479447062
ishmatov.rus <ishmatov.rus@gmail.com>
2023-09-19 15:13:13 +0500

    ViewSet

--- cats/views.py
+++ cats/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 
 from .models import Cat
 from .serializers import CatSerializer
@@ -60,3 +61,21 @@ class APICatDetail(APIView):
         cat = self.get_object(pk)
         cat.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+# class CommentViewSet(viewsets.ModelViewSet):
+#     serializer_class = CommentSerializer
+#     # queryset во вьюсете не указываем
+#     # Нам тут нужны не все комментарии, а только связанные с котом с id=cat_id
+#     # Поэтому нужно переопределить метод get_queryset и применить фильтр
+#     def get_queryset(self):
+#         # Получаем id котика из эндпоинта
+#         cat_id = self.kwargs.get("cat_id")
+#         # И отбираем только нужные комментарии
+#         new_queryset = Comment.objects.filter(cat=cat_id)
+#         return new_queryset
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from cats.views import CatList, CatDetail, APICat, APICatDetail
+from cats.views import CatList, CatDetail, APICat, APICatDetail, CatViewSet
+
+router = DefaultRouter()
+router.register('cats_vs', CatViewSet, basename='tiger')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('cats/', CatList.as_view()),
     path('cats/<int:pk>/', CatDetail.as_view()),
     path('api/v1/posts/', APICat.as_view()),
     path('api/v1/posts/<int:pk>/', APICatDetail.as_view()),
+
 ]
+
