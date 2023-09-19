1cd32976733c5bc64b7287cb36cd154609321277
ishmatov.rus <ishmatov.rus@gmail.com>
2023-09-19 12:09:01 +0500

    APIView

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Cat
 from .serializers import CatSerializer
@@ -12,3 +15,48 @@ class CatList(generics.ListCreateAPIView):
 class CatDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APICatDetail(APIView):
+    def get_object(self, pk):
+        return Cat.objects.get(pk=pk)
+
+    def get(self, request, pk, format=None):
+        cat = self.get_object(pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        cat = self.get_object(pk)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        cat = self.get_object(pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cat = self.get_object(pk)
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from cats.views import CatList, CatDetail
+from cats.views import CatList, CatDetail, APICat, APICatDetail
 
 urlpatterns = [
     path('cats/', CatList.as_view()),
     path('cats/<int:pk>/', CatDetail.as_view()),
+    path('api/v1/posts/', APICat.as_view()),
+    path('api/v1/posts/<int:pk>/', APICatDetail.as_view()),
 ]
